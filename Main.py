b7a973152658a2d796fbdbadfb202d5a8d480954
NewbieProcess <mysticuser0501@gmail.com>
2025-07-17 21:38:58 +0700

    Update Main.py

--- Main.py
+++ Main.py
@@ -13,8 +13,8 @@ SEC_MODEL_PATH = "DiseaseDetect.keras"
 SEC_CLASS_NAMES = ["Healthy", "Pinguecula", "Pterygium Stage 1 (Trace-Mild)", "Pterygium Stage 2 (Moderate-Severe)"]
 
 # Thresholds
-CONFIDENCE_THRESHOLD = 0.50
-MARGIN_THRESHOLD = 0.10
+CONFIDENCE_THRESHOLD = 0.60
+MARGIN_THRESHOLD = 0.15
 
 # --- Page Configuration ---
 st.set_page_config(
