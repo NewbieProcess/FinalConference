3418f84be3df3af2e3884f6d51bbc8922fbdd179
NewbieProcess <mysticuser0501@gmail.com>
2025-08-11 18:40:53 +0700

    Update Main.py

--- Main.py
+++ Main.py
@@ -7,9 +7,9 @@ from streamlit_cropper import st_cropper
 from PIL import Image
 
 # --- Constants ---
-FIRST_MODEL_PATH = "EyeImageDetect.keras"
+FIRST_MODEL_PATH = "EyeDetect.keras"
 FIRST_CLASS_NAMES = ["Eye Detected", "No Eye Detected"]
-SEC_MODEL_PATH = "HPP1P2Detect.keras"
+SEC_MODEL_PATH = "EyeAnalysis.keras"
 SEC_CLASS_NAMES = ["Healthy", "Pinguecula", "Pterygium Stage 1 (Trace-Mild)", "Pterygium Stage 2 (Moderate-Severe)"]
 
 # Thresholds
